74690e1cacd131d86a42ff9279b5122b565c1cee
rafaelgpedroso <rafaelgpedroso@gmail.com>
2025-03-25 12:57:13 -0300

    Arquivo docker compose, aplicação node e nginx conf

---
+++ README.md
@@ -0,0 +1,2 @@
+# Curso Full Cycle 3.0 - Desafio Docker Nginx Node Mysql
+Lista de nomes cadastrada no banco de dados.
---
+++ node/index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const mysql = require('mysql')
+
+const connection = mysql.createConnection({
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database: 'nodedb'
+});
+
+// Rota para listar os registros
+app.get('/', (req, res) => {
+    connection.query(`INSERT INTO people(name) values('Rafael')`)
+    connection.query(`INSERT INTO people(name) values('Giovani')`)
+    
+    connection.query('SELECT name FROM people', (error, results) => {
+        if (error) {
+            return res.status(500).send('Erro na consulta: ' + error);
+        }
+
+        // Monta a string HTML com cabeçalhos e registros
+        let html = `
+            <h1>Full Cycle Rocks!</h1>
+            <h2>People</h2>
+        `;
+
+        html += results.map(row => `<p>${row.name}</p>`).join('');
+
+        res.send(html);
+    });
+});
+
+app.listen(port, ()=>{
+    console.log('Rodando na porta ' + port)
+})
